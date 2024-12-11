fc4172bde1e6239ce356f9bc3d5490cfc2ee32b6
Joshei <joshuaeirm@gmail.com>
2024-12-11 14:20:32 -0500

    commit - text written correctly

--- src/card.jsx
+++ src/card.jsx
@@ -5,7 +5,7 @@ const cards = [
     id: 1,
     title: "Character Jumps Game",
     name: "Josh Eir",
-    info: "the info goes here the info goes here",
+    info: "the info goes here the info goes here the info goeinfo goes heroes here the info goes here the info goes info goes her info goes here info goes hero goes here",
     imageUrl: require("./assets/image.jpg"),
     link: "Code-listing1",
   },
@@ -14,7 +14,7 @@ const cards = [
     id: 2,
     title: "Character Jumps Game",
     name: "Josh Eir",
-    info: "the info goes here",
+    info: "the info goes here the info goes here the info goes here the info goeinfo goes heroes here the info goes here the info goes info goes her info goes here info goes hero g",
     imageUrl: require("./assets/image.jpg"),
     link: "Code-listing2",
   },
@@ -33,23 +33,30 @@ const cards = [
 
 const MainDisplay = () => {
   return (
-    <div>
+    <div className = "top">
       <div>
         {cards.map((card) => (
           <div>
             <div className="title">{card.title}</div>
             <div className="main">
+              <div className = "image" >
               <img src={card.imageUrl} />
-
+              </div>
               <div className="linkOnSide2">
+                
+                
                 <div className="linkOnSide">
+                  <div>
                   <div className="card-info">{card.info}</div>
+                  </div>
                   <div>
                     <Link className="top-list" to={`/${card.link}`}>
                       See the code for this project
                     </Link>
                   </div>
                 </div>
+
+
               </div>
             </div>
           </div>
